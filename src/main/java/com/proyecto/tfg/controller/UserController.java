5d6f762b79ac6370fd162cdb523e69ce3c9f9c9b
Julian <jrcarave91@gmail.com>
2022-09-06 22:31:56 +0200

    prueba controller

--- src/main/java/com/proyecto/tfg/controller/UserController.java
+++ src/main/java/com/proyecto/tfg/controller/UserController.java
@@ -12,15 +12,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.proyecto.tfg.component.mapper.user.UserMapper;
 import com.proyecto.tfg.dto.ApiErrorDTO;
@@ -35,6 +27,7 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 
+@CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true")
 @RestController
 @RequestMapping(value= "/user")
 public class UserController {
